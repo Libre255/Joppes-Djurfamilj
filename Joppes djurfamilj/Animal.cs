b44b48f76e82304b35d4366df8b0f699125dfb6a
Libre255 <briangbv@outlook.com>
2022-04-12 23:01:11 +0200

    implemented energy system at animal class

--- Joppes djurfamilj/Animal.cs
+++ Joppes djurfamilj/Animal.cs
@@ -10,7 +10,7 @@ namespace JoppesDjurFamilj
 		protected string _fav_food = "";
 		protected string _breed = "";
 		protected bool _hungry = new Random().Next(0, 2) == 1 ? true : false;
-
+        protected int Energy = 3;
         public int ID { get { return _id; } set { _id = value; } }
 		public abstract int Age { get; set; }
 		public virtual int Months { get; set; }
@@ -41,7 +41,7 @@ namespace JoppesDjurFamilj
         {
             if (Hungry)
             {
-                WriteLine($"The Cat {Name} is hungry and dont want to play!");
+                WriteLine($"{Name} is hungry and dont want to play!*");
             }
             else
             {
@@ -49,15 +49,23 @@ namespace JoppesDjurFamilj
                 {
                     WriteLine("The laser doesnt have battery please recharge");
                 }
-                playLaser.lower_battery();
-                WriteLine($"The Cat {Name} is playing laser chasing with Joppe");
+                else
+                {
+                    playLaser.lower_battery();
+                    WriteLine($"The Cat {Name} is playing laser chasing with Joppe");
+                    Energy--;
+                    if (Energy == 0)
+                    {
+                        Hungry = true;
+                    }
+                }
             }
         }
 		public virtual void Interact(Ball playBall)
         {
             if (Hungry)
             {
-                WriteLine($"*{Name} is hungry and dont want to play!");
+                WriteLine($"*{Name} is hungry and dont want to play!*");
             }
             else
             {
@@ -69,19 +77,31 @@ namespace JoppesDjurFamilj
                 {
                     playBall.lower_quality(2);
                     WriteLine($"*{Name} is playing fetch ball with Joppe*");
+                    Energy--;
+                    if(Energy == 0)
+                    {
+                        Hungry = true;
+                    }
                 }
             }
         }
 
 		public void eat(string food){
-			if(food.ToLower() != Fav_Food.ToLower())
+            if (Hungry)
             {
-				hungry_animal();
+                if(food.ToLower() != Fav_Food.ToLower())
+                {
+				    hungry_animal();
+                }
+                else
+                {
+				    Hungry = false;
+				    WriteLine("*eated food* and is now full");
+                }
             }
             else
             {
-				Hungry = false;
-				WriteLine("*eated food* and is now full");
+                WriteLine($"{Name} is full and dont want more food right now");
             }
 
         }
